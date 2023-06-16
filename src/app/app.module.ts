8023b5868cec91c3b5b8929481de24296bb1f326
Mikecamdo <mcdoherty@smu.edu>
2023-06-15 23:35:41 -0500

    Added color and font
    Added the color and font of the classic Star Wars text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCrawlComponent } from './create-crawl/create-crawl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCrawlComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-crawl/create-crawl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-crawl',
+  templateUrl: './create-crawl.component.html',
+  styleUrls: ['./create-crawl.component.css']
+})
+export class CreateCrawlComponent {
+
+}
