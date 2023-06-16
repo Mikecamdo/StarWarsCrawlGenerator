4ebaf5d813f0db63502c9940561a2aceee5a2d35
Mikecamdo <mcdoherty@smu.edu>
2023-06-16 00:25:08 -0500

    Added Creation Template
    Added a template for creating a custom Star Wars crawl. Added example info from Episode IX crawl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateCrawlComponent } from './create-crawl/create-crawl.component';
@@ -12,7 +12,8 @@ import { CreateCrawlComponent } from './create-crawl/create-crawl.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-crawl/create-crawl.component.ts
+++ src/app/create-crawl/create-crawl.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-crawl.component.css']
 })
 export class CreateCrawlComponent {
+  intro: string = 'A long time ago in a galaxy far, far away...';
+  logo: string = 'Star Wars';
+  title1: string = 'Episode IX';
+  title2: string = 'THE RISE OF SKYWALKER';
+  body: string = 'The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE. \nGENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER. \nMeanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....';
 
 }
