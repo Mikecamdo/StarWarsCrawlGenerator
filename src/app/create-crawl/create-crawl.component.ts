e161d5d5724768e46dfa294cde6461b38e017af6
Mikecamdo <mcdoherty@smu.edu>
2023-06-27 15:17:52 -0500

    Multi-line string
    Changed the body string to be on multiple lines within the code (to help with code readability)

--- src/app/create-crawl/create-crawl.component.ts
+++ src/app/create-crawl/create-crawl.component.ts
@@ -10,6 +10,9 @@ export class CreateCrawlComponent {
   logo: string = 'Star Wars';
   title1: string = 'Episode IX';
   title2: string = 'THE RISE OF SKYWALKER';
-  body: string = 'The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE. \nGENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER. \nMeanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....';
+  body: string = 
+  `The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE.
+GENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER.
+Meanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....`;
 
 }
