9d2a84c0c7aeda772d97ee33ed4eedebbb10bcf6
Mikecamdo <mcdoherty@smu.edu>
2023-06-27 16:20:41 -0500

    Multiple Templates
    Began implementing the ability to choose from all 9 Star Wars movies' opening crawl to use as a template

--- src/app/create-crawl/create-crawl.component.ts
+++ src/app/create-crawl/create-crawl.component.ts
@@ -8,11 +8,66 @@ import { Component } from '@angular/core';
 export class CreateCrawlComponent {
   intro: string = 'A long time ago in a galaxy far, far away...';
   logo: string = 'Star Wars';
-  title1: string = 'Episode IX';
-  title2: string = 'THE RISE OF SKYWALKER';
-  body: string = 
-  `The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE.
-GENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER.
-Meanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....`;
+  currentMovieTemplate: MovieTemplate = {
+    title1: '',
+    title2: '',
+    body: ""
+  };
 
+  movieTemplates: MovieTemplate[] = [
+    {
+      title1: 'Episode I',
+      title2: 'THE PHANTOM MENANCE',
+      body: "1"
+    },
+    {
+      title1: 'Episode II',
+      title2: 'ATTACK OF THE CLONES',
+      body: "2"
+    },
+    {
+      title1: 'Episode III',
+      title2: 'REVENGE OF THE SITH',
+      body: "3"
+    },
+    {
+      title1: 'Episode IV',
+      title2: 'A NEW HOPE',
+      body: "4"
+    },
+    {
+      title1: 'Episode V',
+      title2: 'THE EMPIRE STRIKES BACK',
+      body: "5"
+    },
+    {
+      title1: 'Episode VI',
+      title2: 'RETURN OF THE JEDI',
+      body: "6"
+    },
+    {
+      title1: 'Episode VII',
+      title2: 'THE FORCE AWAKENS',
+      body: "7"
+    },
+    {
+      title1: 'Episode VIII',
+      title2: 'THE LAST JEDI',
+      body: "8"
+    },
+    {
+      title1: 'Episode IX',
+      title2: 'THE RISE OF SKYWALKER',
+      body: 
+      'The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE.' +
+      '\nGENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER.' +
+      '\nMeanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....'
+    }
+  ]
+}
+
+interface MovieTemplate {
+  title1: string;
+  title2: string;
+  body: string;
 }
